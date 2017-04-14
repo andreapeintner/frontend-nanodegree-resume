35d3591bce34a39021cd4b842d10c80c31146772
andi <andreapeintner@gmail.com>
2017-04-14 16:02:04 +0200

    Add files via upload

---
+++ resumeBuilder.js
@@ -0,0 +1,11 @@
+/*
+This is empty on purpose! Your code to build the resume will go here.
+ */
+
+ $("#main").append("Andi");
+ var awesomeThoughts = "I am Andi and I am awesome!";
+ console.log(awesomeThoughts);
+ 
+ var funThoughts =
+	awesomeThoughts.replace("awesome", "fun");
+$("#main").append(funThoughts);
